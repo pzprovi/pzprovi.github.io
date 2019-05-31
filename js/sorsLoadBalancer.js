4751f0bd3f9ce74a646e1e7865490f67fd532ac0
pzprovi <34093281+pzprovi@users.noreply.github.com>
2019-05-31 12:49:07 +0300

    Update sorsLoadBalancer.js

--- js/sorsLoadBalancer.js
+++ js/sorsLoadBalancer.js
@@ -106,7 +106,7 @@
 					Balancer._mirrorCheck++;
 					// window.location.href = urlPage1 + queryParametars;
 					
-					Balancer.redirectToFastMirror(_mirrorCheck,queryParametars,
+					Balancer.redirectToFastMirror(Balancer._mirrorCheck,queryParametars,
 								 urlPage1,dtBeging1,dtEnd1,
 								 urlPage2,dtBeging2,dtEnd2,
 								 urlPage3,dtBeging3,dtEnd3,
