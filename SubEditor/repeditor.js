24f290ed3b1b20257cb03c8d6f26a3da5465eff0
Sors <sors.pet@gmail.com>
2019-12-16 20:47:47 +0200

    Update repeditor.js

--- SubEditor/repeditor.js
+++ SubEditor/repeditor.js
@@ -3,7 +3,7 @@
 	{					   	  
 	    $('#repeditor').append('<div class="input-group-mb-2">'+
 	                           '   <div class="row"> '+
-	                           '     <div class="input-group-prepend col-4"> '+
+	                           '     <div class="input-group-prepend col-5"> '+
 	                           '        <span class="input-group-text" id="'+id+'">'+id+'</span> '+
 	                           '        <span class="input-group-text">'+w1+'</span> '+
 	                           '        <span class="input-group-text">'+w2+'</span> '+
