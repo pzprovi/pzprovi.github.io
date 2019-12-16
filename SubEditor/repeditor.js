8f0eb8527d9eaabb926239b20ff514496a0269aa
Sors <sors.pet@gmail.com>
2019-12-16 21:09:47 +0200

    Update repeditor.js

--- SubEditor/repeditor.js
+++ SubEditor/repeditor.js
@@ -3,14 +3,19 @@
 	{					   	  
 	    $('#repeditor').append('<div class="input-group-mb-2">'+
 	                           '   <div class="row"> '+
-	                           '     <div class="input-group-prepend col-3"> '+
+	                           '     <div class="input-group-prepend col-5"> '+
 	                           '        <span class="input-group-text" id="'+id+'">'+id+'</span> '+
 	                           '        <span class="input-group-text">'+w1+'</span> '+
 	                           '        <span class="input-group-text">'+w2+'</span> '+
-	                           '     </div> '+
-	                           '     <textarea id="v'+id+'" name="v'+id+'" class="form-control-col-1" rows="2" placeholder="" required>'+w3+'</textarea> '+
+	                       
+                                   '     <textarea id="v'+id+'" name="v'+id+'" class="form-control" rows="2" placeholder="" required>'+w3+'</textarea> '+
 	                           '     <textarea class="form-control" rows="2" placeholder="" required>'+w4+'</textarea> '+
-	                           '  </div>'+
+	                         
+
+                                   '     </div> '+
+
+	                           
+                                   '  </div>'+
 	                           '</div>');	    	    	 
 	}
 
