4b8497704a7d1bcc7c2d0ac82c48e281603d92f4
pzprovi <34093281+pzprovi@users.noreply.github.com>
2019-12-17 14:32:34 +0200

    Update repeditor.js

--- SubEditor/repeditor.js
+++ SubEditor/repeditor.js
@@ -1,24 +1,18 @@
-﻿
+
 	function addNewRow(id, w1, w2, w3, w4, w5)
 	{					   	  
-	    $('#repeditor').append('<div class="input-group-mb-2">'+
-	                           '   <div class="row"> '+
-	                           '     <div class="input-group-prepend col-5"> '+
-	                           '        <span class="input-group-text" id="'+id+'">'+id+'</span> '+
-	                           '        <span class="input-group-text">'+w1+'</span> '+
-	                           '        <span class="input-group-text">'+w2+'</span> '+
-	                       
-                                   '     <textarea id="v'+id+'" name="v'+id+'" class="form-control" rows="2" placeholder="" required>'+w3+'</textarea> '+
-	                           '     <textarea class="form-control" rows="2" placeholder="" required>'+w4+'</textarea> '+
-	                         
-
-                                   '     </div> '+
-
-	                           
-                                   '  </div>'+
+	    $('#repeditor').append('<div class="input-group">'+
+	                           '    <div class="row"> '+
+	                           '        <div class="input-group-prepend"> '+
+	                           '            <span class="input-group-text" id="'+id+'">'+id+'</span> '+
+	                           '            <span class="input-group-text col-sm-2">'+w1+'</span> '+
+	                           '            <span class="input-group-text">'+w2+'</span> '+	                          
+	                           '      		<textarea id="v'+id+'" name="v'+id+'" class="form-control col-5" rows="2" placeholder="" required>'+w3+'</textarea> '+
+	                           '   			<textarea class="form-control" rows="2" placeholder="" required>'+w4+'</textarea> '+
+	                           '  		</div>'+
+	                           '	</div> '+
 	                           '</div>');	    	    	 
 	}
-
 
 	function load()
 	{
@@ -36,8 +30,7 @@
 	}
 
 	jQuery(document).ready(function($) 
-	{
-				
+	{			
 		clear();
 				
 	    var id = 1;
@@ -49,5 +42,4 @@
 		addNewRow(id, w1, w2, w3, w4, w5);
 		
 		var rr = save(id);
-
     });
